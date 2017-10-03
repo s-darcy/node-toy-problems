685e28a65492a5c06d25dd77a27b2c8aa6ad0d76
Shane <sdarcy@redventures.com>
2017-10-03 12:29:50 -0400

    got the employees to be 200

--- server.js
+++ server.js
@@ -9,25 +9,40 @@
 // GET::myendpointname.com/employees/<employeeID> = Json with the information from that specific employee.
 
 const express = require('express');
-const db = require('./db.json'); 
+var db = require('./db.json'); 
 
 var app = express();
 var router = express.Router();
 
-//When you hit the endpoint with a GET request we want the api to respond with all data on the employees. 
-router.get('db/:id', (req, res, next) => {
-  console.log(req.params.id)
-  res.render('special')
+//1. When you hit the endpoint with a GET request we want the api to respond with all data on the employees. 
+router.get('/employees', (req, res) => {
+  console.log(res.json());
 });
 
-//If you hit the endpoint with their employeeID, we want to hand up only the information on that one employee.
+// //2. If you hit the endpoint with their employeeID, we want to hand up only the information on that one employee.
+// router.get('/db/employees/:id', (req, res) => {
+//   var id = req.params.id;
+  
+//   employees.findById(id).then((employees) => {
+//         if (!employees){
+//             return res.status(404).send();
+//         }
+//         res.send({employees});
+//     }).catch((e) => {
+//         res.status(400).send();
+//     });
+// });
+
+
+//3. If you hit the endpoint with an incorrect employeeID, 
+// send back the correct HTTP status code and an error message stating that the employee was not found.
+
 
 //find()
 
 
 // mount the router on the app
 app.use('/', router);
-
 
 app.listen(3000, () => {
     console.log(`Started on port 3000`);
